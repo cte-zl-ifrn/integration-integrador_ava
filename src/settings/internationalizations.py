3cbf6a2c8306dc9d041abaf57bd91b6b5d0572a3
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2025-07-16 22:08:05 -0300

    fix: internationalization settings

--- src/settings/internationalizations.py
+++ src/settings/internationalizations.py
@@ -5,7 +5,7 @@ from sc4py.env import env, env_as_bool
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
 
-LANGUAGE_CODE = env("DJANGO_USE_I18N", "pt-br")
-TIME_ZONE = env("DJANGO_USE_I18N", "America/Fortaleza")
+LANGUAGE_CODE = env("DJANGO_LANGUAGE_CODE", "pt-br")
+TIME_ZONE = env("DJANGO_TIME_ZONE", "America/Fortaleza")
 USE_I18N = env_as_bool("DJANGO_USE_I18N", True)
 USE_TZ = env_as_bool("DJANGO_USE_TZ", True)
