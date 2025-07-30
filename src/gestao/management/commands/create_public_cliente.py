7c702e37c858786a7cbd0f4db82f68735d21ac3c
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2025-07-30 19:41:50 -0300

    feat: suporte a multiplos clientes

---
+++ src/gestao/management/commands/create_public_cliente.py
@@ -0,0 +1,16 @@
+from django.core import exceptions
+from django.core.management import call_command
+from django.core.management.base import BaseCommand, CommandError
+from django.db.utils import IntegrityError
+from django.utils.encoding import force_str
+from django_tenants.utils import get_tenant_model, get_tenant_domain_model
+from gestao.models import Cliente, Dominio
+
+class Command(BaseCommand):
+    help = 'Create a tenant'
+
+    def handle(self, *args, **options):
+        cliente = Cliente(schema_name='public', name='Public Client')
+        cliente.save()
+        dominio = Dominio(tenant=cliente, domain='integrador', is_primary=True)
+        dominio.save()
--- src/settings/apps.py
+++ src/settings/apps.py
@@ -8,25 +8,12 @@ LAST_STARTUP = int(datetime.datetime.timestamp(datetime.datetime.now()) * 1000)
 SHOW_SUPPORT_FORM = env_as_bool("SHOW_SUPPORT_FORM", True)
 SHOW_SUPPORT_CHAT = env_as_bool("SHOW_SUPPORT_CHAT", True)
 
-THIRD_APPS = env_as_list(
-    "THIRD_APPS",
-    [
-        "django_tenants", # tenants
-        "import_export",
-        "simple_history",
-        "sass_processor",
-        # "djrichtextfield",
-        "django_json_widget",
-    ],
-)
-
+THIRD_APPS = env_as_list("THIRD_APPS", ["django_tenants", "import_export", "simple_history", "sass_processor", "django_json_widget"])
 try:
     import django_extensions
-
     THIRD_APPS.append("django_extensions")
 except ModuleNotFoundError:
     pass
-
 
 DJANGO_APPS = env_as_list(
     "DJANGO_APPS",
@@ -41,31 +28,7 @@ DJANGO_APPS = env_as_list(
     ],
 )
 HACK_APPS = env_as_list("HACK_APPS", ["hacks"])
-
-
-MY_APPS = env_as_list(
-    "MY_APPS",
-    [
-        'gestao', # tenants
-        'base',
-        'coorte',
-        'edu',
-        "health",
-        'integrador',
-        "security",
-    ],
-)
-
-SHARED_APPS = THIRD_APPS + DJANGO_APPS + HACK_APPS
-TENANT_APPS = [
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    'base',
-    'coorte',
-    'edu',
-    "health",
-    'integrador',
-    "security",
-]
-INSTALLED_APPS = MY_APPS + SHARED_APPS
+MY_APPS = env_as_list("MY_APPS", ['base', 'coorte', 'edu', "health", 'integrador', "security"])
+INSTALLED_APPS = ['gestao'] + MY_APPS + THIRD_APPS + DJANGO_APPS + HACK_APPS
+TENANT_APPS = INSTALLED_APPS
+SHARED_APPS = [x for x in INSTALLED_APPS if x not in MY_APPS]
