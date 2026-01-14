48c19831cf1ff9e144599edeee76e30fd7b64b6e
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2026-01-14 19:38:47 -0300

    fix: url do enviar_diarios/ e do baixar_notas/

--- src/integrador/urls.py
+++ src/integrador/urls.py
@@ -10,6 +10,6 @@ app_name = IntegradorConfig.name
 
 urlpatterns = [
     path("", RedirectView.as_view(url=f"/admin/")),
-    path(f"/{settings.ROOT_URL_PATH}/enviar_diarios/", sync_up_enrolments, name="api_sync_up_enrolments"),
-    path(f"/{settings.ROOT_URL_PATH}/baixar_notas/", sync_down_grades, name="api_sync_down_grades"),
+    path(f"{settings.ROOT_URL_PATH}/enviar_diarios/", sync_up_enrolments, name="api_sync_up_enrolments"),
+    path(f"{settings.ROOT_URL_PATH}/baixar_notas/", sync_down_grades, name="api_sync_down_grades"),
 ]
