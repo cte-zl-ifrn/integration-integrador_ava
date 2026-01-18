9b3fe92a176a096ba715be24753d3074aef49176
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2026-01-18 17:52:48 -0300

    fix, o dsgovbr só rola em dev, por hora.

--- src/settings/apps.py
+++ src/settings/apps.py
@@ -11,5 +11,9 @@ SHOW_SUPPORT_CHAT = env_as_bool("SHOW_SUPPORT_CHAT", True)
 THIRD_APPS = env_as_list("THIRD_APPS", ["import_export", "simple_history", "sass_processor", "django_json_widget", "django_extensions"])
 DJANGO_APPS = [f"django.contrib.{x}" for x in ["admin", "auth", "contenttypes", "sessions", "messages", "staticfiles", "humanize"]]
 MY_APPS = env_as_list("MY_APPS", ["base", "coorte", "edu", "health", "integrador", "security", "dsgovbr"])
+try:
+    import dsgovbr
+except ImportError:
+    MY_APPS.remove("dsgovbr")
 HACK_APPS = env_as_list("HACK_APPS", ["hacks"])
 INSTALLED_APPS = MY_APPS + THIRD_APPS + DJANGO_APPS + HACK_APPS
